c20eb6bf4a6e2b888b80e9972a80a6d153bf4906
Gabriel Augusto <gabrielaugustosb13@gmail.com>
2021-02-03 21:53:22 -0300

    V2.1.2, modificando submenus e menus do navbar

--- scripts.js
+++ scripts.js
@@ -3,11 +3,15 @@ let dropMenu = document.getElementsByClassName('dropdown-content');
 let servicoBtn = document.getElementsByClassName('servico-drop');
 
 for(let i = 0; i < servicoBtn.length; i++){
-		servicoBtn[i].addEventListener('click', function mostraDrop() {
-			if(dropMenu[i].style.display === 'none'){
-				dropMenu[i].style.display = 'block';
+		servicoBtn[i].addEventListener('mouseover', function mostraDrop() {
+			if(dropMenu[0].style.display === 'block' && dropMenu[1].style.display === 'block' && dropMenu[2].style.display === 'block'){
+				dropMenu[0].style.display = 'none';
+				dropMenu[1].style.display = 'none';
+				dropMenu[2].style.display = 'none';
 			} else {
-				dropMenu[i].style.display = 'none';
+				dropMenu[0].style.display = 'block';
+				dropMenu[1].style.display = 'block';
+				dropMenu[2].style.display = 'block';
 			}
 		});
 	}
