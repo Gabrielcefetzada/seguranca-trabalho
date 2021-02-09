83817687c3e781fb57c1b97a75da7a5c4fe5bf13
Gabriel Augusto <gabrielaugustosb13@gmail.com>
2021-02-09 10:55:16 -0300

    V3.0.0 - missao, visao, valores, quem somos, colocando textos oficiais

--- scripts.js
+++ scripts.js
@@ -1,18 +1,40 @@
 
-let dropMenu = document.getElementsByClassName('dropdown-content');
-let servicoBtn = document.getElementsByClassName('servico-drop');
 
-for(let i = 0; i < servicoBtn.length; i++){
-		servicoBtn[i].addEventListener('mouseover', function mostraDrop() {
-			if(dropMenu[0].style.display === 'block' && dropMenu[1].style.display === 'block' && dropMenu[2].style.display === 'block'){
-				dropMenu[0].style.display = 'none';
-				dropMenu[1].style.display = 'none';
-				dropMenu[2].style.display = 'none';
-			} else {
-				dropMenu[0].style.display = 'block';
-				dropMenu[1].style.display = 'block';
-				dropMenu[2].style.display = 'block';
-			}
-		});
-	}
+let elementoServico = document.getElementsByClassName('elemento-servico'); 
+let pServicos = document.getElementsByClassName('p-servico');
+let pServicos2 = document.getElementsByClassName('p-servico2');
 
+for(let i = 0; i < pServicos.length; i++){
+	if(pServicos[i].className == 'p-servico') {
+   pServicos[i].innerHTML = pServicos[i].innerHTML.substring(0,100); // limitar caracteres
+ }
+}
+
+for(let i = 0; i < pServicos2.length; i++){
+	if(pServicos2[i].className == 'p-servico2') {
+   pServicos2[i].innerHTML = pServicos2[i].innerHTML.substring(0,2000); // expandir caracteres
+ }
+}
+
+for(let i = 0; i < elementoServico.length; i++){
+	elementoServico[i].addEventListener('click', function expandeElemento(){ // expandir elementos
+		if(elementoServico[i].style.height === '30rem'){
+			elementoServico[i].style.height = '8rem';		
+				pServicos2[i].style.display = 'none';
+				pServicos[i].style.display = 'block';
+
+		} else {
+			elementoServico[i].style.height = '30rem';
+				pServicos2[i].style.display = 'block';
+				pServicos[i].style.display = 'none';
+		}		
+	})
+}
+
+// tirar letras em maiusculo - PRONTO
+// bug do submenu nos contatos - fazer com css - PRONTO
+// textos grandes lá - problema tamanho div expandida
+// redes sociais
+// inserir links nos submenus - PRONTO
+// corrigir opções do select do form de contato
+// Logo
